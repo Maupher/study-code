cda4ba137107e1f693c880173341503d0b35289a
maupher <ibach.simon-it25@dhbw-ravensburg.de>
2026-01-31 00:31:34 +0100

    firstGrid

--- courses/coding-in-C/Challenges/chessGame.c
+++ courses/coding-in-C/Challenges/chessGame.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void main() 
 {
@@ -16,26 +17,51 @@ void init_GUI()
 {
     short sGrid = 8;    // max. 25 (A - Z)
     
-    system("cls");
+    system("cls"); 
+    printf("\n\n");
 
     buildGrid(sGrid);
-
-    printf("\n\t\t%c\n\t\t%c", cHoriz, cVert);
+    bool test = 0;
 
 }
 
 void buildGrid(short size)
 {
-    char cHoriz = 0x7C; // Horizontale Linie 
-    char cVert = 0x90;  // Vertikale Linie 
+    // char cHoriz = 0x7C; // Horizontale Linie 
+    // char cVert = 0x90;  // Vertikale Linie 
+    // printf("\n\t\t%c\n\t\t%c", cHoriz, cVert);
+    
+    for (int i = 0; i < size; i++)
+    {
+        printf("\n\t");
+        gridHorizontal(size);
+        printf("\n\t");
+        gridVertical(size + 1, (i%2));      //i % 2 -> Black / White - gerade / ungerade
+    }
 
-    for ()
 }
 
 void gridHorizontal(short length)
 {
+    char cHoriz = 0x7C; // Horizontale Linie
+    char cSpace = 0x20; // Space 
+    for (int i = 0; i < length; i++)
+    {
+        putchar(cHoriz);
+        putchar(cSpace);
+    }
+} 
+
+void gridVertical(short length, bool color)
+{
+    char cVert = 0x90;  // Vertikale Linie
+    char[4] cSpace = {0x20, 0x20, 0x20}; // Space
+    
+
+    // for right color in der auswahl (i+color) % 2 --> wenn color 1 beginnt mit schwarz, wenn color 0 beginnt mit weiß und wechselt
     for (int i = 0; i < length; i++)
     {
         
+        putchar(cVert);
     }
-} 
+}
