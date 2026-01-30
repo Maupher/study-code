7035547ccee344404224fa5e7de67d0bfa647a30
maupher <ibach.simon-it25@dhbw-ravensburg.de>
2026-01-31 00:01:55 +0100

    start

---
+++ courses/coding-in-C/Challenges/chessGame.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void main() 
+{
+
+init_GUI();
+
+
+}
+
+
+
+
+
+void init_GUI()
+{
+    short sGrid = 8;    // max. 25 (A - Z)
+    
+    system("cls");
+
+    buildGrid(sGrid);
+
+    printf("\n\t\t%c\n\t\t%c", cHoriz, cVert);
+
+}
+
+void buildGrid(short size)
+{
+    char cHoriz = 0x7C; // Horizontale Linie 
+    char cVert = 0x90;  // Vertikale Linie 
+
+    for ()
+}
+
+void gridHorizontal(short length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+} 
