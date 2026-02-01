64d99f36342d455bee690262941b116da7935b1e
maupher <ibach.simon-it25@dhbw-ravensburg.de>
2026-02-01 01:03:38 +0100

    bug fixes

--- courses/coding-in-C/Challenges/chessGame.c
+++ courses/coding-in-C/Challenges/chessGame.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool bLayout[25][25][2]; 
+
+void init_GUI();
+void buildGrid(short size);
+void gridHorizontal(short length);
+void gridVertical(short length, bool color);
+void gridLettering(short length);
 
 void main() 
 {
@@ -23,27 +31,27 @@ void init_GUI()
 
     buildGrid(sGrid);
     bool test = 0;
-
+    
 }
 
 void buildGrid(short size)
 {
-    
+    gridLettering(size);
     for (int i = 0; i < size; i++)
     {
-        printf("\n\t");
         gridHorizontal(size);
-        printf("\n\t");
-        gridVertical(size + 1, (i%2));      //i % 2 -> White / Black - gerade / ungerade
+        gridVertical(size, (i%2));      //i % 2 -> White / Black - gerade / ungerade
     }
     gridHorizontal(size);
+    gridLettering(size);
 }
 
 void gridHorizontal(short length)
 {
-    char cHoriz = 0x7C; // Horizontale Linie
+    char cHoriz = 0x5F; // Horizontale Linie
     char cSpace[] = {0x20}; // Space
 
+    printf("\n\t");
     for (int i = 0; i < length; i++)
     {
         printf(cSpace);
@@ -51,19 +59,42 @@ void gridHorizontal(short length)
     }
 } 
 
+//add Nummerierung!!
 void gridVertical(short length, bool color)
 {
-    char cVert = 0x90;  // Vertikale Linie
+    char cVert = 0x7C;  // Vertikale Linie
     char cBlack = 0x23; // "color" Black
     char cSpace[] = {0x20, 0x20, 0x20}; // Space
     
+    printf("\n\t");
     // for right color in der auswahl (i+color) % 2 --> wenn color 1 beginnt mit schwarz, wenn color 0 beginnt mit weiß und wechselt
     for (int i = 0; i < length; i++)
     {
         putchar(cVert);
         if ((i + color) % 2 == 0)
+        { 
             printf(cSpace);
-        else 
-            printf(" %c ", cBlack);
+        }
+        else if ((i + color) % 2 == 1)
+        {
+            putchar(cBlack);
+        }
+    }
+    putchar(cVert);
+}
+
+void gridLettering(short length)
+{
+    uint8_t A = 0b01000001; 
+    char cSpace[] = {0x20}; // Space
+
+    printf("\n\t");
+    for (int i = 0; i < length; i++)
+    {
+        printf(cSpace);
+        putchar(A);
+        A++;
+        printf(cSpace);
     }
 }
+ 
