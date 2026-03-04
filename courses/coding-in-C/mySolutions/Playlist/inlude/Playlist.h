1ecb5b8f6c776116507c4569b842d74f6d3422a9
Toaster.-. <no@no.com>
2026-03-04 16:31:57 +0100

    Playlist start

--- courses/coding-in-C/mySolutions/Playlist/inlude/Playlist.h
+++ courses/coding-in-C/mySolutions/Playlist/inlude/Playlist.h
@@ -6,30 +6,50 @@ typedef struct Song Song;
 
 
 static const uint8_t stdCharLength = 30;
+static const uint8_t maxSongs = 500;
 
 struct Playlist {    
     Song* p_head;
     Song* p_tail;
     uint16_t anz_Songs;
     uint64_t total_playtime; //define in seconds
-    char Playlist_name[stdCharlength];
-    char created_by[stdCharlength];
+    char Playlist_name[stdCharLength];
+    char created_by[stdCharLength];
     time_t created_time;
 };
 
 struct Song{
-    Playlist* p_playlist;
+    const Playlist* p_playlist;
     Song* p_prev;
     Song* p_next;
 
     uint16_t id;
-    char c_Song[30];
-    char c_artist[30];
+    char c_name[stdCharLength];
+    char c_artist[stdCharLength];
     uint16_t playtime; //defined in seconds
+    time_t timestamp_add;
 };
 
 
-Playlist* Playlist_create();
+void Playlist_create(Playlist* p_self, char Playlist_name, char created_by, time_t timestamp);
 
 //Funktion for creating a Song
-Song* Song_create(Playlist* p_playlist, char c_name, char c_artist, uint16_t playtime);
+void Song_create(Song* p_self, const Playlist* p_playlist, char c_name, char c_artist, uint16_t playtime, time_t timestamp);
+
+//add Function for adding to Playlist 
+int Song_add(const Playlist* p_playlist, Song* p_song);
+
+//get an Song by its ID 
+Song* Song_get_by_id(const Playlist* p_playlist, uint16_t id);
+
+//get an Song by it's name
+Song* Song_get_by_name(const Playlist* p_playlist, char name);
+
+//get an Song by thr artist
+Song* Song_get_by_artist(const Playlist* p_playlist, char Artist);
+
+//Deleting and freeing Song
+int Song_delete(const Playlist* p_playlist, Song* p_song);
+
+//Free Playlist 
+int Playlist_delete(Playlist* playlist);
--- courses/coding-in-C/mySolutions/Playlist/src/playlist.c
+++ courses/coding-in-C/mySolutions/Playlist/src/playlist.c
@@ -1,25 +1,42 @@
 #include <stdlib.h>
-#include "../inlude"
+#include "../inlude/Playlist.h"
 
-Playlist* Playlist_create(char Playlist_name, char created_by, time_t timestamp);
+void Playlist_create(char Playlist_name, char created_by, time_t timestamp){
+    
+};
 
 //Funktion for creating a Song
-Song* Song_create(Playlist* p_playlist, char c_name, char c_artist, uint16_t playtime, time_t timestamp);
+void Song_create(Playlist* p_playlist, char c_name, char c_artist, uint16_t playtime, time_t timestamp){
+
+
+};
 
 //add Function for adding to Playlist 
-int Song_add(Playlist* p_playlist, Song* p_song);
+int Song_add(Playlist* p_playlist, Song* p_song){
+
+};
 
 //get an Song by its ID 
-Song* Song_get_by_id(Playlist* p_playlist, uint16_t id);
+Song* Song_get_by_id(Playlist* p_playlist, uint16_t id){
+
+};
 
 //get an Song by it's name
-Song* Song_get_by_id(Playlist* p_playlist, char name);
+Song* Song_get_by_name(Playlist* p_playlist, char name){
+
+};
 
 //get an Song by thr artist
-Song* Song_get_by_artist(Playlist* p_playlist, char Artist);
+Song* Song_get_by_artist(Playlist* p_playlist, char Artist){
+
+};
 
 //Deleting and freeing Song
-int Song_delete(Playlist* p_playlist, Song* p_song);
+int Song_delete(Playlist* p_playlist, Song* p_song){
+
+};
 
 //Free Playlist 
-int Playlist_delete(Playlist* playlist);
+int Playlist_delete(Playlist* playlist){
+
+};
